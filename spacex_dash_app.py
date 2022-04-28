450ee0f67d5aa6a5d4805ca91d01961fc2699265
Wei Wan <wanviv@gmail.com>
2022-04-28 17:46:27 -0400

    week4 prediction

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -37,13 +37,14 @@ app.layout = html.Div(children=[html.H1('SpaceX Launch Records Dashboard',
                                 html.Br(),
 
                                 html.P("Payload range (Kg):"),
+                                # html.P("testing payload range"),
                                 # TASK 3: Add a slider to select payload range
                                 #dcc.RangeSlider(id='payload-slider',...)
-                                dcc.RangeSlider(id='plyload-slider', min=0, max = 10000, step=1000,\
+                                dcc.RangeSlider(id='payload-slider', min=0, max = 10000, step=1000,\
                                     marks ={0:'0 kg', 1000:'1000 kg',2000:'2000 kg',3000:'3000 kg',\
                                         4000:'4000 kg',5000:'5000 kg',6000:'6000 kg',\
                                         7000:'7000 kg',8000:'8000 kg',9000:'9000 kg',10000:'10000 kg'},\
-                                        value=[min_payload, max_payload],),
+                                        value=[min_payload, max_payload]),
 
                                 # TASK 4: Add a scatter chart to show the correlation between payload and launch success
                                 html.Div(dcc.Graph(id='success-payload-scatter-chart')),
@@ -66,11 +67,10 @@ def get_pie_chart(entered_site):
 
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
-@app.callback(
-     Output(component_id='success-payload-scatter-chart',component_property='figure'),
-     [Input(component_id='site_dropdown',component_property='value'),Input(component_id="payload_slider", component_property="value")]
+@app.callback(Output(component_id='success-payload-scatter-chart',component_property='figure'),
+     [Input(component_id='site-dropdown',component_property='value'),Input(component_id="payload-slider", component_property="value")]
 )
-def update_scattergraph(entered_site,payload_slider):
+def get_scatter_chart(entered_site,payload_slider):
     if entered_site == 'All Sites':
         low, high = payload_slider
         df  = spacex_df
